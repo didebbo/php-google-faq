5a4c5ad07a16d178b45e65ea9ddc0e529c2d74b4
didebbo <gianluca.napoletano.93@gmail.com>
2021-10-20 17:02:56 +0200

    aggiunsto css base

--- index.php
+++ index.php
@@ -51,12 +51,8 @@ $db = [
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
     <title>Google | Domande Frequenti</title>
-    <style>
-        .response ul {
-            list-style: none;
-        }
-    </style>
 </head>
 
 <body>
